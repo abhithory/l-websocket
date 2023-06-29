c6ca0f90b0d32676ce7eead5fa730fc2e5ccd2a7
theteamisbest <abhithory@gmail.com>
2023-06-29 22:05:03 +0530

    proper connection added with socketio + only send message to one person

--- backend/index.js
+++ backend/index.js
@@ -9,23 +9,27 @@ app.use(cors()); // Enable CORS for Express.js
 const server = http.createServer(app);
 
 const io = socketIO(server, {
-  cors: {
-    origin: 'http://localhost:3000', // Replace with your allowed origin
-    methods: ['GET', 'POST'], // Specify the allowed HTTP methods
-  },
+    cors: {
+        origin: 'http://localhost:3000', // Replace with your allowed origin
+        methods: ['GET', 'POST'], // Specify the allowed HTTP methods
+    },
 });
 
 io.on('connection', socket => {
-  socket.on('message-from-client', message => {
-    socket.broadcast.emit('message-from-server', message); 
-  });
+    socket.on('message-from-client', (message, receiver) => {
+        // if (receiver) {
+        socket.to(receiver).emit('message-from-server', message);
+        // }else{
+        //     socket.broadcast.emit('message-from-server', message);
+        // }
+    });
 
-//   socket.on('disconnect', () => {
-//     console.log('A user disconnected');
-//   });
+    //   socket.on('disconnect', () => {
+    //     console.log('A user disconnected');
+    //   });
 });
 
 const port = 3001;
 server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
--- fronend/src/App.js
+++ fronend/src/App.js
@@ -2,20 +2,34 @@ import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
 
-const socket = io('http://localhost:3001'); 
+const socket = io('http://localhost:3001');
 function App() {
   const [messages, setMessages] = useState([]);
+  const [socketId, setSocketId] = useState("")
+
+  const [to, setTo] = useState("")
 
 
   const handleSendMessage = async (e) => {
     e.preventDefault();
     console.log("sending msg");
     setMessages(prevMessages => [...prevMessages, "sent msg:: " + e.target.msg.value]);
-    socket.emit("message-from-client", e.target.msg.value);
+    socket.emit("message-from-client", e.target.msg.value, to);
   }
 
+  useEffect(() => {
+    // Listen for the 'connect' even
+    console.log("aaa");
+    socket.on('connect', () => {
+      console.log('Connected to Socket.IO server');
+      console.log('Socket ID:', socket.id);
+      setSocketId(socket.id)
+    });
 
-  
+  }, []);
+
+
+
   useEffect(() => {
     // setSocketId(socket.id)
     socket.on("message-from-server", (msg) => {
@@ -29,11 +43,13 @@ function App() {
 
   return (
     <div>
+      <h6>userid: {socketId}</h6>
+      <span>receiver address</span>
+      <input onChange={(e) => setTo(e.target.value)} type="text" placeholder='enter receiver address' />
+
+
       <form onSubmit={handleSendMessage}>
 
-        <br />
-        <h6>{socket?.id}</h6>
-        <br />
         <input id='msg' name='msg' type="text" placeholder='enter your message' />
         <button type='submit'>Connect to server using websocket</button>
 
